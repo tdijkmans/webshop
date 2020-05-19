b29a53a5fa199caea69c2ddeff7f720a545f1a4a
tdijkmans <tdijkmans@gmail.com>
2020-05-19 15:31:48 +0200

    basic pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavLink
+        to="/"
+        activeStyle={{
+          fontWeight: "bold",
+          color: "red",
+        }}
+      >
+        Awesome Webshop
+      </NavLink>
+
+      <NavLink
+        to="/cart"
+        activeStyle={{
+          fontWeight: "bold",
+          color: "red",
+        }}
+      >
+        2 products in cart
+      </NavLink>
+
+      <Switch></Switch>
     </div>
   );
 }
